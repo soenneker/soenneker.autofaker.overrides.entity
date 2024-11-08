434826b52fb1211a90560118302e2aa061c2844e
Jake Soenneker <jake@soenneker.com>
2024-11-08 09:01:34 -0600

    Initial commit

---
+++ src/EntityOverride.cs
@@ -0,0 +1,28 @@
+using Soenneker.Utils.AutoBogus.Context;
+using Soenneker.Utils.AutoBogus.Override;
+using System;
+
+namespace Soenneker.AutoFaker.Overrides.Entity;
+
+/// <summary>
+/// An AutoFaker (AutoBogus) override for the object Entity
+/// </summary>
+public class EntityOverride : AutoFakerOverride<Entities.Entity.Entity>
+{
+    public override bool CanOverride(AutoFakerContext context)
+    {
+        return context.GenerateType.IsSubclassOf(typeof(Entities.Entity.Entity));
+    }
+
+    public override void Generate(AutoFakerOverrideContext context)
+    {
+        var target = (context.Instance as Entities.Entity.Entity)!;
+
+        // These can be overriden with more specific type overrides
+        target.Id = context.Faker.Random.Guid().ToString();
+
+        DateTime utcNow = DateTime.UtcNow;
+        target.CreatedAt = utcNow;
+        target.ModifiedAt = utcNow;
+    }
+}
---
+++ test/Soenneker.AutoFaker.Overrides.Entity.Tests/EntityOverrideTests.cs
@@ -0,0 +1,5 @@
+namespace Soenneker.AutoFaker.Overrides.Entity.Tests;
+
+public class EntityOverrideTests
+{
+}
